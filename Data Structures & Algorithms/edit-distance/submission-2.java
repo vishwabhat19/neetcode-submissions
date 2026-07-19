c72d271485ae883055efcdba80f2827c594a4c71
Vishwanath Bhat <36367514+vishwabhat19@users.noreply.github.com>
2026-07-19 20:11:11 +0530

    Add: edit-distance - submission-2

---
+++ Data Structures & Algorithms/edit-distance/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        Integer[][] dp = new Integer[word1.length() + 1][word2.length() + 1];
+        return solve(word1, word2, 0, 0, dp);
+    }
+
+    int solve(String word1, String word2, int i, int j, Integer[][] dp) {
+        if (i == word1.length()) {
+            return word2.length() - j;
+        }
+        if (j == word2.length()) {
+            return word1.length() - i;
+        }
+
+        if (dp[i][j] != null) return dp[i][j];
+
+        if (word1.charAt(i) == word2.charAt(j)) {
+            return solve(word1, word2, i+1, j+1, dp);
+        }
+
+        int insert = solve(word1, word2, i, j+1, dp);
+        int delete = solve(word1, word2, i+1, j, dp); //Deleted a char, check the next char
+        int replace = solve(word1, word2, i+1, j+1, dp);
+        int min = Math.min(insert, Math.min(delete, replace));
+        dp[i][j] = (1 + min);
+        return 1 + min;
+    }
+}
