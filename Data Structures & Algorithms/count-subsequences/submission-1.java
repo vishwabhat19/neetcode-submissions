3c9ecaf4b03f7af7a9dd7ba045cdc095a59d7086
Vishwanath Bhat <36367514+vishwabhat19@users.noreply.github.com>
2026-07-19 17:31:21 +0530

    Add: count-subsequences - submission-1

---
+++ Data Structures & Algorithms/count-subsequences/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        Integer[][] dp = new Integer[s.length() + 1][t.length() + 1];
+        return solve(s, t, 0, 0, dp);
+    }
+
+    int solve(String s, String t, int i, int j, Integer[][] dp) {
+        if (j == t.length()) return 1;
+
+        if (i == s.length()) return 0;
+
+        if (dp[i][j] != null) return dp[i][j];
+
+        int ways = 0;
+
+        if (s.charAt(i) == t.charAt(j)) {
+            ways += solve(s, t, i+1, j+1, dp);
+        }
+
+        ways+= solve(s, t, i+1, j, dp);
+
+        dp[i][j] = ways;
+
+        return ways;
+
+    }
+}
